404d56ace707f0a0333fffbf684b387078153ea5
maarten <vercammen.maarten.2000@gmail.com>
2022-06-28 20:37:15 +0200

    add user

--- model/user.ts
+++ model/user.ts
@@ -2,17 +2,18 @@
 import { user } from "../types";
 
 async function getUsers(onResult: (users: user[], err: Error) => void) {
-  const query = `select   birthdate
-  city
-  email
-  firstname
-  id
-  lastname
-  number
-  password
-  phone
-  postalcode
-  street
+  const query = `select 
+  birthdate,
+  city,
+  "email",
+  firstname,
+  id,
+  lastname,
+  number,
+  password,
+  phone,
+  postalcode,
+  street,
   typename  
   from ksa.user inner join ksa.usertype using(id)`;
   try {
@@ -23,4 +24,16 @@ async function getUsers(onResult: (users: user[], err: Error) => void) {
   }
 }
 
-export { getUsers };
+async function addUser(user: user, onResult: (err: Error) => void) {
+  const query = `INSERT into ksa.user(firstname, lastname, birthdate, email, phone, street, number, postalcode, city, password, usertype)
+  values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, (select id from ksa.usertype where typename = $11))`
+    try{
+      var {rows} = await connectionPool.query(query, [user.firstname, user.lastname, user.birthdate, user.email, user.phone, user.street, user.number, user.postalcode, user.city, user.password, user.typename])
+      onResult(null)
+    }
+    catch(err){
+      onResult(err)
+    }
+}
+
+export { getUsers, addUser};
--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,5 +1,6 @@
 ﻿import express, { Request, Response, Handler } from "express";
 import * as userModel from "../model/user";
+import { user } from '../types';
 
 const userRouter = express.Router();
 
@@ -12,5 +13,18 @@ userRouter.get("/", (res: Response, req: Request) => {
     }
   });
 });
+
+userRouter.post('/add', (req: Request, res: Response) => {
+  var user = <user>req.body;
+  console.log
+  userModel.addUser(user, (err) =>{
+    if(err){
+      res.status(500).json({ message: err.message, type: "error" })
+    }
+    else{
+      res.status(200).json({message: 'user added succesfuly', type:'succes'})
+    }
+  })
+})
 
 export default userRouter;
