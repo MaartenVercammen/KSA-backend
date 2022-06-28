5cac43b380f7faf7013a75f55c72e9cb47f27f7a
maarten <vercammen.maarten.2000@gmail.com>
2022-06-28 16:45:43 +0200

    change get users and user type

--- model/user.ts
+++ model/user.ts
@@ -2,7 +2,19 @@
 import { user } from "../types";
 
 async function getUsers(onResult: (users: user[], err: Error) => void) {
-  const query = "select * from ksa.user inner join ksa.usertype using(id)";
+  const query = `select   birthdate
+  city
+  email
+  firstname
+  id
+  lastname
+  number
+  password
+  phone
+  postalcode
+  street
+  typename  
+  from ksa.user inner join ksa.usertype using(id)`;
   try {
     var { rows } = await connectionPool.query(query);
     onResult(<user[]>rows, null);
