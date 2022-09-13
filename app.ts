41e6c626a15cd2eff27de0fdef939726d68eb5c1
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-09-13 16:17:21 +0200

    update show react

--- app.ts
+++ app.ts
@@ -3,17 +3,26 @@ import express from "express";
 import cors from "cors";
 import * as bodyParser from "body-parser";
 import userRouter from "./routes/userRouter";
+import path from 'path'
 
 const app = express();
 dotenv.config();
 
+const react = path.join(__dirname,'build')
+
+app.use(express.static(react))
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/user", userRouter);
 
+
 app.get("/status", (req, res) => {
   res.json({ message: "Backend is running..." });
 });
+
+app.get("*", (req, res) =>{
+  res.sendFile(path.join(react, "index.html"))
+  })
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on port ${process.env.APP_PORT}.`);
