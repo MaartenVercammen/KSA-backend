4987653c75a07e9f83341155ac2b96152ad45929
CrlH <10894454+CrlH@users.noreply.github.com>
2022-10-24 14:24:28 +0200

    [API] review, fix and enhance PostRouter

--- model/postModel.ts
+++ model/postModel.ts
@@ -1,18 +1,31 @@
 import { v4 as uuid } from 'uuid';
 import type { Post as IPost } from '../types';
 import knex from '../database/knexconfig';
+import newLogger from '../modules/logger';
+
+const logger = newLogger('model.Post');
 
 export default class {
   static async create(post: IPost): Promise<IPost> {
+    logger.silly('[create] Hello!');
     return knex('posts').insert({
       ...post,
       id: uuid(),
     });
   }
 
-  static async getAll(): Promise<IPost[]> { return knex<IPost>('posts').select('*'); }
+  static async getAll(): Promise<IPost[]> {
+    logger.silly('[getAll] Hello!');
+    return knex<IPost>('posts').select('*');
+  }
 
-  static async update(post: IPost): Promise<number> { return knex('posts').update(post).where('id', post.id); }
+  static async update(post: IPost): Promise<number> {
+    logger.silly('[update] Hello!');
+    return knex('posts').update(post).where('id', post.id);
+  }
 
-  static async deleteById(id: string): Promise<number> { return knex('posts').delete().where({ id }); }
+  static async deleteById(id: string): Promise<number> {
+    logger.silly('[update] Hello!');
+    return knex('posts').delete().where({ id });
+  }
 }
--- routes/postRouter.ts
+++ routes/postRouter.ts
@@ -1,56 +1,71 @@
 import express from 'express';
 import { accessVerification } from '../modules/middlewares';
-import { Post as IPost, Roles } from '../types';
+import { Roles } from '../types';
 import Post from '../model/postModel';
+import newLogger from '../modules/logger';
 
 const postRouter = express.Router();
+const logger = newLogger('router.post');
 
 postRouter.get('/', async (_req, res) => {
   try {
+    logger.silly('[get] Hello!');
     const posts = await Post.getAll();
     return res.status(200).json(posts);
   } catch (err: unknown) {
     if (!(err instanceof Error)) {
+      logger.error('[get] Critical error occurred', err);
       return res.status(500).json({ message: 'Unknown error occurred!', type: 'error' });
     }
+    logger.error(`[get] ${err.message}`);
     return res.status(500).json({ message: err.message, type: 'error' });
   }
 });
 postRouter.post('/', accessVerification(Roles.ADMIN, Roles.BONDS, Roles.BRAGGEL), async (req, res) => {
-  const post: IPost = req.body;
   try {
+    logger.silly('[post] Hello!');
+    const { body: { post } } = req;
     await Post.create(post);
     return res.status(200).json({ type: 'ok', message: 'post created' });
   } catch (err: unknown) {
     if (!(err instanceof Error)) {
+      logger.error('[post] Critical error occurred', err);
       return res.status(500).json({ message: 'Unknown error occurred!', type: 'error' });
     }
+    logger.error(`[post] ${err.message}`);
     return res.status(500).json({ type: 'error', message: err.message });
   }
 });
 
 postRouter.put('/', accessVerification(Roles.ADMIN, Roles.BONDS, Roles.BRAGGEL), async (req, res) => {
-  const post: IPost = req.body;
   try {
+    logger.silly('[put] Hello!');
+    const { body: { post } } = req;
     await Post.update(post);
     return res.status(200).json({ type: 'ok', message: 'post updated' });
   } catch (err: unknown) {
     if (!(err instanceof Error)) {
+      logger.error('[put] Critical error occurred', err);
       return res.status(500).json({ message: 'Unknown error occurred!', type: 'error' });
     }
+    logger.error(`[put] ${err.message}`);
     return res.status(500).json({ type: 'error', message: err.message });
   }
 });
 
 postRouter.delete('/', accessVerification(Roles.ADMIN, Roles.BONDS, Roles.BRAGGEL), async (req, res) => {
-  const id = <string>req.query['id'];
   try {
+    logger.silly('[delete] Hello!');
+    const { body: { post: { id, title } } } = req;
+    logger.debug('[delete] Post with title `%s` (id: `%s`) will be deleted!', title, id);
     await Post.deleteById(id);
     return res.status(200).json({ type: 'ok', message: 'post deleted' });
   } catch (err: unknown) {
     if (!(err instanceof Error)) {
+      logger.error('[delete] Critical error occurred', err);
       return res.status(500).json({ message: 'Unknown error occurred!', type: 'error' });
     }
+    logger.error(`[delete] ${err.message}`);
     return res.status(500).json({ type: 'error', message: err.message });
   }
 });
